7c3d8b37bbf4bdcfa4be0607c20e7721e9454c77
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-02-15 20:07:46 +0000

    Undo some test changes

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
+//import './index.css';
+import App from './App';
 import Sentimental from './pages/Sentimental';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sentimental />
+    <App />
   </React.StrictMode>
 );
 
