853687dacfff2aa1fd71ea31566942fa9aaf22e4
kellyd92 <127953277+kellyd92@users.noreply.github.com>
2024-02-23 11:05:52 +0000

    Update Header.js
    Made it compatible with my stylesheet "stylez.css"

--- frontend/my-app/src/components/Header.js
+++ frontend/my-app/src/components/Header.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import '../components/Header.css'
+import React from 'react';
+import '../components/Header.css';
+import '../components/stylez.css';
+import logo from './CigarMan.png';
 
-export default function Header(){
 
-    return(
-      <> 
-        <div className='container'>
-          <div className='content'>
-            {/* row 1 */}
-            <div className='row'>
-              <div className='button-group-1'>
-                <a type='button' className='headerlink' href="/">
-                  Main
-                </a>
-                <a type='button' className='headerlink' href="/sentimental">
-                  Sentimental Analysis
-                </a>
-                <a type='button' className='headerlink' href="/image">
-                  Image Analysis
-                </a>
-              </div>
-            </div>
-            <hr/>
+function Header() {
+  return (
+    <>
+   
+      <section className="sub-header">
+        <nav>
+          <a href="alt.html"><img src={logo} alt="Cigar Man3" /></a>
+          <div className="navlinks">
+            <ul>
+              <li><a type='button' className='headerlink' href="/">Main</a></li>
+              <li><a type='button' className='headerlink' href="/sentimental">Sentimental Analysis</a></li>
+              <li><a type='button' className='headerlink' href="/image">Image Analysis</a></li>
+            
+            </ul>
           </div>
-        </div>
-      </>
-    )
+        </nav>
+      </section>
+    </>
+  );
 }
+
+export default Header;
