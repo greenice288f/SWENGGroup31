73cccfc9e6c71ce1fc9c3a23afc2aba099b18681
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-02-26 12:00:04 +0000

    Implemented new agreement popup, fixed a warning in CSS.

--- frontend/my-app/src/pages/Main.js
+++ frontend/my-app/src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import AgreementPopup from '../components/AgreementPopup';
 
 function Main () {
 
     return (
         <>
             <Header></Header>
+            <AgreementPopup></AgreementPopup>
             <div style={{
                 display: 'flex',
                 alignItems: 'center',
