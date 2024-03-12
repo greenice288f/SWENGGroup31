5d35675d83cb8b3e2a46a51a1c7a394c4e722f9f
Phan Binh Nguyen <119639983+greenice288f@users.noreply.github.com>
2024-03-12 15:19:40 +0000

    deleted unnecessary files

--- backend/test2.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-with open("./backend/test.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode()
-with open('encoded_string.txt', 'w') as file:
-    file.write(encoded_string)
-print('sending reply all done')
