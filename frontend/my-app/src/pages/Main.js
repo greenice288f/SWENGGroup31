f552ae43f62e3851ffcdd1285688cd6d0c155e68
James Gray <grayj1@tcd.ie>
2024-03-22 17:16:07 +0000

    Fixed footer problems on main page

--- frontend/my-app/src/pages/Main.js
+++ frontend/my-app/src/pages/Main.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import AgreementPopup from '../components/AgreementPopup';
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import AgreementPopup from "../components/AgreementPopup";
+import "./pages.css";
 
-function Main () {
-
-    return (
-        <>
-            <Header />
-            <AgreementPopup />
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                paddingTop: "130px", /* Remove later -> made for demo sake*/
-                paddingBottom: "300px", /* Remove later -> made for demo sake*/
-                justifyContent: 'center',
-            }}>
-                <h1>Welcome!</h1>
-            </div>
-            <Footer />
-        </>
-    );
+function Main() {
+  return (
+    <div class="page-container">
+      <Header />
+      <AgreementPopup />
+      <body>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h2>Welcome!</h2>
+          <h2>Log-in to your instagram below to generate a report.</h2>
+        </div>
+      </body>
+      <Footer />
+    </div>
+  );
 }
 
 export default Main;
