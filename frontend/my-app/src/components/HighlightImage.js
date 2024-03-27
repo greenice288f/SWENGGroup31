ab274bb344029a0751ed314b23dd4d7e8242ebfc
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-03-27 22:02:37 +0000

    removed some old files (oops)

--- frontend/my-app/src/components/HighlightImage.js
+++
@@ -1,9 +0,0 @@
-export default function HighlightImage({src}, {x}, {y}, {alt}){
-    return (
-    <>
-        <img src={src} alt={alt}>
-            <div className="rectangle" />
-        </img>
-    </> 
-    )
-}
