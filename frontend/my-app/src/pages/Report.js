b4b45302a74d6c8b3ee9d64ff8dd516ebd719460
James Gray <grayj1@tcd.ie>
2024-03-30 22:48:42 +0000

    Added report generation from instagram api call

--- frontend/my-app/src/pages/Report.js
+++ frontend/my-app/src/pages/Report.js
@@ -3,14 +3,17 @@ import ScoreBar from "../components/ScoreBar";
 import ScoreRing from "../components/ScoreRing";
 import "./report.css";
 
-function Report() {
+function Report({ smoker_report }) {
   const img_src = require("../components/CigarMan.png");
 
   const [name, setName] = useState("James Gray");
   const [username, setUsername] = useState("undefined");
-  const [images, setImages] = useState(Array(3).fill(img_src));
+  const [images, setImages] = useState(JSON.parse(smoker_report.images));
   const [score, setScore] = useState(70);
   const [date, setDate] = useState("1/1/0000");
+
+  const base64images = JSON.parse(smoker_report.images);
+  const imageDescriptions = JSON.parse(smoker_report.info);
 
   let scoreDescription =
     score > 60
@@ -18,6 +21,29 @@ function Report() {
       : score > 20
         ? "Based on this Instagram account's score, they may be a smoker!"
         : "Based on this Instagram account's score, they are unlikely to be a smoker!";
+
+  let descriptions = [];
+  let certainties = [];
+  for (let i = 0; i < base64images.length; i++) {
+    let description = "";
+    let descriptiveValue = 0;
+    let len = imageDescriptions[0][i].length;
+    if (imageDescriptions[0][i][len - 2] === 0) {
+      description = "no evidence of smoking";
+      descriptiveValue = 0;
+    } else if (imageDescriptions[0][i][len - 2] === 1) {
+      description = "evidence of smoking, cigarette near face";
+      descriptiveValue = imageDescriptions[0][i][0];
+    } else if (imageDescriptions[0][i][len - 2] === 2) {
+      description = "evidence of smoking, cigarette near hand";
+      descriptiveValue = imageDescriptions[0][i][0];
+    } else {
+      description = "evidence of smoking, only cigarette detected";
+      descriptiveValue = imageDescriptions[0][i][0];
+    }
+    descriptions.push(description);
+    certainties.push(descriptiveValue);
+  }
 
   return (
     <>
@@ -40,15 +66,18 @@ function Report() {
         </div>
         <div class="image-container">
           <div class="image-cards">
-            {images.map((image) => (
+            {images.map((base64String, index) => (
               <div class="card image-card">
-                <img src={image} alt="" style={{ width: "50%" }} />
+                <img
+                  key={index}
+                  src={`data:image/png;base64,${base64String}`}
+                  alt=""
+                  style={{ width: "50%" }}
+                />
                 <div class="image-desc">
-                  <ScoreRing score={score} />
+                  <ScoreRing score={certainties[index] * 100} />
                   <h4>Notes:</h4>
-                  <div>
-                    Three cigarettes detected. Cigarette detected near face...
-                  </div>
+                  <div>{descriptions[index]}</div>
                 </div>
               </div>
             ))}
